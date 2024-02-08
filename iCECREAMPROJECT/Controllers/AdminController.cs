7e0b0950e9c58bad944905ae408cf22b792ebe92
Tamilarasi0326 <s.tamilarasi.0207@gmail.com>
2024-02-08 08:16:32 +0530

    code created

---
+++ iCECREAMPROJECT/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using iCECREAMPROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iCECREAMPROJECT.Controllers
+{
+    public class AdminController : Controller
+    {
+        ICECREAMPROJECTEntities db = new ICECREAMPROJECTEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Feedback()
+        {
+            return View(db.tbl_feedback.ToList().OrderByDescending(x=>x.f_id));
+        }
+    }
+}
---
+++ iCECREAMPROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iCECREAMPROJECT.MvcApplication" Language="C#" %>
